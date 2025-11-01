dd48c52b471317f8b0504a91a1cd39091ac64c94
Isaac Rodriguez <isaac_024@hotmail.com>
2025-10-31 19:58:26 -0600

    Def Variables

--- ExamenI/PrimerScript.R
+++ ExamenI/PrimerScript.R
@@ -4,3 +4,8 @@ resultado <- numero_base + 5
 print(resultado)
 
 #Definicion variable
+x <- 23
+ls()        #Listar variables cargadas
+rm(x)       #Remover variable
+rm(list = ls()) # Remover todas las variables
+
