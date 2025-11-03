18a208b756a59410441989964acf527b0a43891c
Isaac Rodriguez <isaac_024@hotmail.com>
2025-11-03 09:23:49 -0600

    While ciclo

--- ExamenI/while.R
+++ ExamenI/while.R
@@ -4,3 +4,17 @@ while (cuenta <= 10) {
   print(paste("Cuenta es:", cuenta))
   cuenta <- cuenta + 1
 }
+#%% Sentencia while conta
+conta <- 1
+while (conta <= 5) {
+  print(paste("Contador vale:", conta))
+  conta <- conta + 1
+}
+#%% Numero telefeno
+telefono <- readline(prompt = "Ingrese su número telefónico (8 dígitos): ")
+
+#>while (nchar(telefono) != 8 || !grepl("^[0-9]+$", telefono)) {
+#>while (nchar(telefono) != 8 {
+#>  telefono <- readline(prompt = "Número inválido. Ingrese un número telefónico
+#>  válido (8 dígitos): ")
+#>}
