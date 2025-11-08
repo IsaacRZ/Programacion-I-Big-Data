932cb1e2bfac31d9572182f50f5d078284802118
Isaac Rodriguez <isaac_024@hotmail.com>
2025-11-07 21:25:48 -0600

    Registro de archivos movidos a nueva carpeta Condicionales/

--- ExamenI/Tipos de datos/DataFrames.R
+++ ExamenI/Tipos de datos/DataFrames.R
@@ -1,4 +1,50 @@
+setwd("~/Big Data/ProgramacionI")
 #DATAFRAME
 #Estructura 2D 
 #HETEROGENEA (Columnas con distintos tipos de datos:bool,int,float,chr)
 #Lista cada componente es una columna(vectores, matrices, listas)
+
+alumnos <- data.frame(
+   nombre = c("Ana", "Luis", "Pedro"),
+   edad = c(22, 25, 24),
+   aprobado = c(TRUE, TRUE, FALSE)
+   )
+
+alumnos
+#Acceso a columnas 
+alumnos$nombre
+alumnos$edad
+alumnos$aprobado
+
+#Acceso elementos
+alumnos[1,] #Filas
+alumnos[,2] #Columnas
+alumnos[2,3] #Elemento row2, col3
+
+#Nueva COL
+alumnos$nota <- c(8.5, 9.0, 6.5)
+
+#Filtar filas 
+subset(alumnos, aprobado == TRUE)
+
+#VER Estructura
+str(alumnos)
+summary(alumnos)
+
+#PRACTICA
+nombres <- c("Ana", "Juan", "Sara", "Luis")
+edades <- c(28, 35, 22, 41)
+activo <- c(TRUE, TRUE, FALSE, TRUE)
+
+empleados <- data.frame(
+  Nombre = nombres,
+  Edad = edades,
+  Activo = activo
+)
+
+empleados$Edad
+mean(empleados$Edad)
+
+empleados[empleados$Activo == TRUE,]
+subset(empleados, Activo == TRUE )
+
